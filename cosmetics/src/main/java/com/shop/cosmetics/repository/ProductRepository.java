46ea7d74098c45e6bf5ab90010e889b3d49fa376
nastenalikee-byte <nastena.likee@gmail.com>
2026-02-19 22:10:46 +0300

    Delete cosmetics directory

--- cosmetics/src/main/java/com/shop/cosmetics/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shop.cosmetics.repository;
-
-import com.shop.cosmetics.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
